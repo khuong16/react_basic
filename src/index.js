306926f82bf7a5ef35bf709e00588398c3289587
khuong16 <khuong16>
2022-01-10 22:37:29 +0700

    video39_setUp_redux

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ import { createStore } from 'redux'
 import rootReducer from './store/reducers/rootReducer';
 
 // khai báo thằng redux store.
-const reduxStore = createStore(rootReducer);
+const reduxStore = createStore(rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
